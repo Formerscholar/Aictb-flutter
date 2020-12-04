8c74d48437a22c13f9bce746e0fe94ef1f5bf5e5
Formerscholar <2319315514@qq.com>
2020-12-04 21:47:04 +0800

    add 1

---
+++ lib/api/NWApi.dart
@@ -0,0 +1,5 @@
+class NWApi {
+  static final baseApi = "https://api.aictb.com/v3/";
+  static final teacherLogin =
+      "teacher/wx_teacher_login"; //接口返回：{"code": int, "message": "String", "data": {"account": "String", "password": "String"}}
+}
